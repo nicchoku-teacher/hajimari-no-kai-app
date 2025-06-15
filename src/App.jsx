01c679e0409ca5a664668d3c9f237a42a8a0e3ea
nicchoku-teacher <endoh-k@edu-g.gsn.ed.jp>
2025-06-15 09:20:30 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import AnniversaryPage from "./AnniversaryPage";
+import React, { useState } from "react";
+import AnniversarySelectPage from "./pages/AnniversarySelectPage";
+import AnniversaryDisplayPage from "./pages/AnniversaryDisplayPage";
 
 export default function App() {
-  return <AnniversaryPage />;
+  const [selected, setSelected] = useState([]);
+  const [showDisplay, setShowDisplay] = useState(false);
+
+  return showDisplay ? (
+    <AnniversaryDisplayPage selected={selected} />
+  ) : (
+    <AnniversarySelectPage selected={selected} setSelected={setSelected} onConfirm={() => setShowDisplay(true)} />
+  );
 }
